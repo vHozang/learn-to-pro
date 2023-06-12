44b520b0143732c0208c641b88219b9f151ffcbc
Baymaxx <vu.ngochoang2808@gmail.com>
2023-06-12 13:46:05 +0700

    12/6/23

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ExpenseItem from "./componets/ExpenseItem";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Learn</p>
+        <ExpenseItem></ExpenseItem>
       </header>
     </div>
   );
